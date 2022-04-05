f4d5ca09b57536cff57636fd828226ea6e2ba7e6
jakobwallisch <93660746+jakobwallisch@users.noreply.github.com>
2022-04-05 15:09:42 +0200

    ScreenBuilder wurde zum Projekt hinzugefügt.
    Es wurde eine Titel und ein Icon zur "Stage" (Fenster) hinzugefügt.
    Außerdem wurde eine Controller-Klasse für den HomeScreen (Scene) erstellt.

--- travel-diary/diary/src/main/java/at/jku/se/diary/HelloFX.java
+++ travel-diary/diary/src/main/java/at/jku/se/diary/HelloFX.java
@@ -6,20 +6,53 @@
 package at.jku.se.diary;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.File;
+
 
 public class HelloFX extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello World, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+
+/*        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample.fxml"));
+        fxmlLoader.setRoot(new AnchorPane());
+        Parent root = fxmlLoader.load();*/
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/HomeScreen.fxml"));
+        Parent root = loader.load();
+
+        //Parent root = new AnchorPane();
+
+
+     //Titel des Fensters (der Stage)
+        stage.setTitle("Reisetagebuch");
+
+        //Setzt Icon des Fensters (der Stage)
+        Image icon = new Image("img.png");
+        stage.getIcons().add(icon);
+
+        // -----Größe der Stage festlegen-----
+        //stage.setHeight(420);
+        //stage.setWidth(420);
+
+        // -----Resizable true or false-----
+        //stage.setResizable(false);
+
+        // -----Vollbild ein- oder ausschalten
+        //stage.setFullScreen(true);
+
+        stage.setScene(new Scene(root));
+
+        // Methode, damit die Stage angezeigt wird
         stage.show();
     }
 
--- travel-diary/diary/src/main/java/module-info.java
+++ travel-diary/diary/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module diary {
     requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.fxml;
 
     opens at.jku.se.diary;
 }
