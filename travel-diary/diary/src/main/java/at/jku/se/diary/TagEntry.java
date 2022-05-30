c995d5c79ec3ae8fe0f8b9e7b185a94c2dfcc667
schindl <f.schindlbauer@gmx.at>
2022-05-30 22:17:32 +0200

    rating exceptions hinzugefügt

--- travel-diary/diary/src/main/java/at/jku/se/diary/TagEntry.java
+++ travel-diary/diary/src/main/java/at/jku/se/diary/TagEntry.java
@@ -14,6 +14,9 @@ public class TagEntry {
         if(rating < 0){
             rating = 0;
         }
+        if(rating > 5){
+            rating = 5;
+        }
         TagEntry tagEntry = new TagEntry();
         tagEntry.setTagText(tagText);
         tagEntry.setTag(tag);
@@ -55,6 +58,7 @@ public class TagEntry {
 
     public void setRating(int rating) {
         if (rating < 0){ this.rating = 0; }
+        else if(rating > 5){ this.rating = 5;}
         else{ this.rating = rating; }
     }
 }
--- travel-diary/diary/src/test/java/at/jku/se/diary/TagEntryTest.java
+++ travel-diary/diary/src/test/java/at/jku/se/diary/TagEntryTest.java
@@ -9,7 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Florian Schindlbauer
  */
 class TagEntryTest {
-
+    /**
+     * Test of createNewTagEntry method, of class CreateDiaryEntryController by using method createNewTagEntry of class TagEntry.
+     */
     @Test
     void createNewTagEntry() throws TagEntryException {
         TagEntry tagEntry = TagEntry.createNewTagEntry("Es war sehr schoen", "Strand", 4);
@@ -18,15 +20,21 @@ class TagEntryTest {
         assertEquals(tagEntry.getRating(), 4);
         TagEntry t = TagEntry.createNewTagEntry("Es war sehr schoen!!", "Hotel", -1);
         assertEquals(t.getRating(), 0);
+        TagEntry t2 = TagEntry.createNewTagEntry("Es war sehr schoen!!!!", "Bar", 6);
+        assertEquals(t2.getRating(), 5);
     }
-
+    /**
+     * Test of getTag method of class TagEntry.
+     */
     @Test
     void getTag() throws TagEntryException {
         TagEntry t = TagEntry.createNewTagEntry("Es war sehr schoen", "Strand", 4);
         assertEquals(t.getTag(), "Strand");
 
     }
-
+    /**
+     * Test of setTag method of class TagEntry.
+     */
     @Test
     void setTag() throws TagEntryException {
         assertThrows(TagEntryException.class, () ->{
@@ -36,7 +44,9 @@ class TagEntryTest {
         t.setTag("neuerTag");
         assertEquals(t.getTag(), "neuerTag");
     }
-
+    /**
+     * Test of getTagText method of class TagEntry.
+     */
     @Test
     void getTagText() throws TagEntryException {
         TagEntry t = TagEntry.createNewTagEntry("Test", "tag", 5);
@@ -44,21 +54,30 @@ class TagEntryTest {
         TagEntry t2 = TagEntry.createNewTagEntry(null, "tag", 5);
         assertEquals(t2.getTagText(), null);
     }
-
+    /**
+     * Test of setTagText method of class TagEntry.
+     */
     @Test
     void setTagText() throws TagEntryException {
         TagEntry t = TagEntry.createNewTagEntry("Test", "tag", 5);
         t.setTagText("neuerText");
         assertEquals(t.getTagText(), "neuerText");
     }
-
+    /**
+     * Test of getStarString method of class TagEntry.
+     */
     @Test
     void getStarString() throws TagEntryException {
         TagEntry t = TagEntry.createNewTagEntry("Test", "tag", 5);
         t.setStarString("test");
         assertEquals(t.getStarString(), "test");
     }
-
+    /**
+     * Test of getStarString method of class TagEntry.
+     * If the right amount of stars appears is already tested in createNewTagEntry class, as after using this method
+     * a for loop prints out the right amount of stars.
+     * this for loop is determined in the method createNewTagEntry so it can not be ignored or passed over!
+     */
     @Test
     void setStarString() throws TagEntryException {
         //Die Funktion, dass die richtige Anzahl an Sternen erscheint, wird bei der Erstellung eines TagEntries autom. überprüft
@@ -66,13 +85,21 @@ class TagEntryTest {
         t.setStarString("test");
         assertEquals(t.getStarString(), "test");
     }
-
+    /**
+     * Test of getRating method of class TagEntry.
+     * This method just return the integer number that you gave the created tag entry as parameter in method
+     * createNewTagEntry
+     */
     @Test
     void getRating() throws TagEntryException {
         TagEntry t = TagEntry.createNewTagEntry("Test", "tag", 5);
         assertEquals(t.getRating(), 5);
     }
-
+    /**
+     * Test of setRating method of class TagEntry.
+     * If the rating is less than one it will automatically be 0.
+     * In case it is greater than 5 it will automatically be set to 5.
+     */
     @Test
     void setRating() throws TagEntryException {
         TagEntry t = TagEntry.createNewTagEntry("Test", "tag", 5);
@@ -80,6 +107,7 @@ class TagEntryTest {
         assertEquals(t.getRating(), 3);
         t.setRating(-1);
         assertEquals(t.getRating(), 0);
-        //rating wird bei negativ gesetztem Wert automatisch auf 0 gesetzt
+        t.setRating(6);
+        assertEquals(t.getRating(), 5);
     }
 }
